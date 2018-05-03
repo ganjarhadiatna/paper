2a9e29f902859d3d37220efff1864532de28b03f
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-05-04 01:21:20 +0700

    changed ui small design

--- resources/views/main/post.blade.php
+++ resources/views/main/post.blade.php
@@ -2,23 +2,7 @@
 	<div class="mid">
 		<div class="mid-tool">
 			<div class="bot-tool">
-				<div class="nts">
-					<button 
-						class="zoom btn btn-circle btn-sekunder-color btn-no-border" 
-						onclick="pictZoom({{ $story->idimage }})"
-						style="float: left; margin-right: 5px;">
-						<span class="fas fa-lg fa-search-plus"></span>
-					</button>
-					@if ($story->id == Auth::id())
-						<a href="{{ url('/paper/'.$story->idpapers.'/design/'.$story->idimage.'/edit') }}">
-							<button 
-								class="zoom btn btn-circle btn-sekunder-color btn-no-border"
-								style="margin-right: 5px;">
-								<span class="fas fa-lg fa-pencil-alt"></span>
-							</button>
-						</a>
-					@endif
-				</div>
+				<div class="nts"></div>
 				<div class="bok">
 					<button class="btn btn-main-color btn-no-border" key="{{ $story->idimage }}" onclick="addBookmark('{{ $story->idimage }}')">
 						@if (is_int($story->is_save))
@@ -29,6 +13,33 @@
 					</button>
 				</div>
 			</div>
+			<div class="oth-tool">
+				<div class="grid grid-2x">
+					<div class="grid-1">
+						<button 
+							class="icn btn btn-circle btn-black-color btn-no-focus" 
+							onclick="pictZoom({{ $story->idimage }})"
+							style="float: left; margin-right: 5px;">
+							<span class="fas fa-lg fa-search-plus"></span>
+						</button>
+						@if ($story->id == Auth::id())
+							<a href="{{ url('/paper/'.$story->idpapers.'/design/'.$story->idimage.'/edit') }}">
+								<button 
+									class="icn btn btn-circle btn-black-color btn-no-focus"
+									style="margin-right: 5px;">
+									<span class="fas fa-lg fa-pencil-alt"></span>
+								</button>
+							</a>
+						@endif
+					</div>
+					<div class="grid-2 right">
+						<button class="icn btn btn-circle btn-black-color btn-no-focus"
+							onclick="opPostSmallPopup('open', 'menu-popup', '{{ $story->idpapers }}', '{{ $story->id }}', '{{ $story->idimage }}')">
+							<span class="fa fa-lg fa-ellipsis-h"></span>
+						</button>
+					</div>
+				</div>
+			</div>
 			<div>
 				<a href="{{ url('/paper/'.$story->idpapers.'/design/'.$story->idimage) }}">
 					<div class="cover"></div>
@@ -37,17 +48,6 @@
 					id="pict-{{ $story->idimage }}"
 					key="{{ $story->idpapers }}">
 				</a>
-			</div>
-			<div class="oth-tool">
-				<div class="grid grid-2x">
-					<div class="grid-1"></div>
-					<div class="grid-2 right">
-						<button class="icn btn btn-circle btn-black-color btn-no-focus"
-							onclick="opPostSmallPopup('open', 'menu-popup', '{{ $story->idpapers }}', '{{ $story->id }}', '{{ $story->idimage }}')">
-							<span class="fa fa-lg fa-ellipsis-h"></span>
-						</button>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
