cc0b04938949d963dd08cdb2219752e8f6c094a3
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-05-05 22:25:40 +0700

    fixed

---
+++ public/js/frame.js
@@ -0,0 +1,5 @@
+var frame = {
+    design: function () {
+        return "<h1>this is post</h1>";
+    }
+}
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -22,6 +22,7 @@
 	<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/jquery-ui.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/watch.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/frame.js') }}"></script>
 	<script type="text/javascript">
 		var iduser = '{{ Auth::id() }}';
 
